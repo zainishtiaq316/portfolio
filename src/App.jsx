7ff33fb746c8f9dcfac6728f6d6dba7d8ac9877d
Zain Ishtiaq <zainishtiaq.7866@gmail.com>
2023-12-06 20:28:25 +0500

    fixed page case.

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Navbar from "./components/Navbar";
 import Themes from "./components/Theme";
 import Home from "./pages/home/Home";
 import About from "./pages/about/About";
-import Portfolio from "./pages/Portfolio/Portfolio";
+import Portfolio from "./pages/portfolio/Portfolio";
 import Contact from "./pages/contact/Contact";
 function App ()  {
   return (
